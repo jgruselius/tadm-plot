532eee35185c1006668127a7d3a0bd9be442f80b
Joel Gruselius <j.gruselius@gmail.com>
2024-03-05 11:32:22 +0100

    Add GUI version (Tkinter)

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
 import sys
 import os
-import logging
-import argparse
-from pathlib import Path
-
-from concurrent.futures import ProcessPoolExecutor, as_completed
-
-from rich.logging import RichHandler
-from InquirerPy import inquirer
-from rich.progress import Progress, TextColumn, BarColumn, MofNCompleteColumn, TimeRemainingColumn
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo, showerror
+from tkinter import filedialog as fd
 
 from tadm import import_tadm_data, get_liquid_class_names, import_tolerance_band_data, \
     merge_tadm_and_tolerance_data, get_data_for_liquid_class, plot_both_steps, check_driver
 
+
 # Extract data from TADM database (.mdb) and plot curves together with tolerance bands
 # Created by: Joel Gruselius <github.com/jgruselius>, 2023-11
-#
-# TODO:
-#  [ ] Handle steps without data
-#  [x] Progress display for '-all'
-#  [x] Detect when ODBC driver is not installed and print guide
-#  [x] Add option for the liquid class database to use
 
 
-# Print instructions for installing ODBC driver:
 def driver_help():
-    logging.error("Could not find a required ODBC driver to read MS Access databases.\n"
-                  "Install the [italic]Microsoft Access Database Engine 2010 Redistributable[/italic] from here:\n"
-                  "https://www.microsoft.com/en-US/download/details.aspx?id=13255")
+    msg1 = "Could not find a required ODBC driver to read MS Access databases."
+    msg2 = "Install the Microsoft Access Database Engine 2010 Redistributable from here:"
+    link = "https://www.microsoft.com/en-US/download/details.aspx?id=13255"
+    print(msg1, msg2, link)
+    showerror(title="Driver not found", message=msg1, detail=f"{msg2} {link}")
 
 
 def get_path(file_name: str) -> str:
@@ -35,150 +26,115 @@ def get_path(file_name: str) -> str:
     return os.path.join(script_dir, file_name)
 
 
-def create_progress_bar() -> Progress:
-    return Progress(
-        TextColumn("[progress.description]{task.description}"),
-        BarColumn(),
-        MofNCompleteColumn(),
-        TextColumn("ETA:"),
-        TimeRemainingColumn(),
-        transient=False
-    )
+class SelectionFrame(ttk.Frame):
+    def __init__(self, container, **options):
+        super().__init__(container, **options)
+
+        self.selected_file = tk.StringVar()
+        self.selected_liquid = tk.StringVar()
+        self.columnconfigure(0, weight=1, minsize=90)
+        self.columnconfigure(1, weight=4, minsize=90*4)
+        self.columnconfigure(2, weight=1, minsize=90)
+
+        self.__create_widgets()
+
+    def __create_widgets(self):
+        # File open label
+        ttk.Label(self, text="TADM file:").grid(column=0, row=0, sticky=tk.W, pady=5)
+        # Selected file entry
+        ttk.Entry(self, textvariable=self.selected_file, state="readonly").grid(column=1, row=0, sticky=tk.W+tk.E, pady=5)
+        # Open button
+        ttk.Button(self, text='Browse', command=self.select_file).grid(column=2, row=0, sticky=tk.E, pady=5)
+        # Import button
+        self.import_button = ttk.Button(self, text='Import', state=tk.DISABLED, command=self._load_data)
+        self.import_button.grid(column=1, row=1, sticky=tk.W, pady=5)
+        # Liquid select label
+        ttk.Label(self, text="Liquid class:").grid(column=0, row=2, sticky=tk.W, pady=25)
+        # Liquid select combobox
+        self.lc_combobox = ttk.Combobox(self, textvariable=self.selected_liquid, state=tk.DISABLED)
+        self.lc_combobox.grid(column=1, row=2, columnspan=2, sticky=tk.W+tk.E, pady=25)
+        # Quit button
+        self.generate_button = ttk.Button(self, text='Exit', command=self.master.destroy)
+        self.generate_button.grid(column=0, row=3, sticky=tk.W, pady=25)
+        # Plot button
+        self.generate_button = ttk.Button(self, text='Generate', command=self._generate_plot, state=tk.DISABLED)
+        self.generate_button.grid(column=2, row=3, sticky=tk.W, pady=25)
+        self.lc_combobox.bind("<<ComboboxSelected>>", lambda x: self.generate_button.configure(state=tk.NORMAL))
+
+        for widget in self.winfo_children():
+            widget.grid(padx=15)
+
+    def select_file(self):
+        filetypes = (
+            ('TADM databse', '*.mdb'),
+            ('All files', '*.*')
+        )
+
+        filename = fd.askopenfilename(
+            title='Open file',
+            initialdir='./',
+            filetypes=filetypes)
+
+        self.selected_file.set(filename)
+        print(self.selected_file.get())
+        self.import_button.config(state=tk.ACTIVE)
+
+    def _load_data(self):
+        self.master.load_data(self.selected_file.get(), get_path("ML_STARLiquids.mdb"))
+        self.lc_combobox.configure(values=list(self.master.lc_names), state="readonly")
+        self.lc_combobox.focus_set()
+        self.import_button.configure(state=tk.DISABLED)
+
+    def _generate_plot(self):
+        lc = self.selected_liquid.get()
+        self.master.generate_plot(lc)
 
 
-def interactive_plot(df, lc_names: list[str], args: argparse.Namespace):
-    while True:
-        lc = inquirer.fuzzy(
-            message="Select liquid:", choices=lc_names,
-            instruction="Type or use the up/down arrow keys",
-            long_instruction="Type a few letters of the liquid class name and press ENTER to select the highlighted value").execute()
-        step_data = get_data_for_liquid_class(df, lc)
-        plot_both_steps(step_data)
-        if not inquirer.confirm("Do you want to generate another plot?").execute():
-            break
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        # configure the root window
+        self.title("TADM plotter")
+        self.geometry("600x230")
+        if os.name == "nt":
+            self.iconbitmap(get_path("icon.ico"))
+        self.configure(background="white")
+        s = ttk.Style()
+        s.configure("TFrame", background="white")
+        s.configure("TLabel", background="white")
+
+        # layout on the root window
+        self.columnconfigure(0, weight=4)
+        self.columnconfigure(1, weight=1)
+
+        self.__create_widgets()
+
+        self.df = None
+        self.lc_names = None
+
+        if not check_driver():
+            driver_help()
+            self.destroy()
+
+    def __create_widgets(self):
+        file_frame = SelectionFrame(self)
+        file_frame.grid(column=0, row=0)
+
+        for widget in self.winfo_children():
+            widget.grid(padx=15, pady=15, sticky=tk.W)
+
+    # This could be done in a separate thread:
+    def load_data(self, tadm_path: str, lcdb_path: str):
+        self.df = import_tadm_data(tadm_path)
+        self.lc_names = get_liquid_class_names(self.df)
+        tol = import_tolerance_band_data(lcdb_path, self.lc_names)
+        self.df = merge_tadm_and_tolerance_data(self.df, tol)
+
+    def generate_plot(self, lc_name: str):
+        step_data = get_data_for_liquid_class(self.df, lc_name)
+        plot_both_steps(step_data, None, False, "tkAgg")
 
 
-def export_all(df, lc_names: list[str], args: argparse.Namespace):
-    progress_bar = create_progress_bar()
-    with progress_bar:
-        for lc in progress_bar.track(lc_names, description="Generating plots..."):
-            logging.debug(f"Getting data for {lc}")
-            step_data = get_data_for_liquid_class(df, lc)
-            file_path = gen_plot_name(args.infile, lc, args.outdir)
-            create_out_dir(file_path.parent)
-            plot_both_steps(step_data, file_path, True)
-
-
-def export_all_parallel(df, lc_names: list[str], args: argparse.Namespace):
-    progress_bar = create_progress_bar()
-    with progress_bar:
-        task = progress_bar.add_task("Generating plots...", total=len(lc_names))
-        with ProcessPoolExecutor() as ex:
-            handles = []
-            for lc in lc_names:
-                logging.debug(f"Getting data for {lc}")
-                step_data = get_data_for_liquid_class(df, lc)
-                file_path = gen_plot_name(args.infile, lc, args.outdir)
-                create_out_dir(file_path.parent)
-                handles.append(ex.submit(plot_both_steps, step_data, file_path, True))
-            for h in as_completed(handles):
-                progress_bar.advance(task)
-
-
-def main(args: argparse.Namespace):
-    if args.outdir:
-        create_out_dir(args.outdir)
-    df = import_tadm_data(args.infile)
-    lc_names = get_liquid_class_names(df)
-    tol = import_tolerance_band_data(args.lcdb, lc_names)
-    df = merge_tadm_and_tolerance_data(df, tol)
-    if args.all:
-        export_all(df, lc_names, args)
-    elif args.par:
-        export_all_parallel(df, lc_names, args)
-    elif args.interactive:
-        interactive_plot(df, lc_names, args)
-    elif args.liquid:
-        assert (any(args.liquid in x for x in lc_names))
-        # NOT IMPLEMENTED
-
-
-def gen_plot_name(source_name: str, liquid_class: str, dir_path: Path) -> Path:
-    p = dir_path / Path(source_name).stem / liquid_class
-    logging.debug(f"Generated plot name: '{p}'")
-    return p.with_suffix(".png")
-
-
-def create_out_dir(path: Path) -> Path:
-    if not path.exists():
-        path.mkdir(parents=False)
-        logging.debug(f"Created directory '{path}'")
-    return path
-
-
-def file_exists(path: str) -> Path:
-    p = Path(path).absolute()
-    if not p.exists():
-        raise argparse.ArgumentTypeError(f"Path does not exist: {path}")
-    if not p.is_file():
-        raise argparse.ArgumentTypeError(f"Path is not a file: {path}")
-    return p
-
-
-def dir_exists(path: str):
-    p = Path(path).absolute()
-    parent = p.parent
-    if not (parent.exists() and parent.is_dir()):
-        raise argparse.ArgumentTypeError(f"Directory does not exist: {parent}")
-    return p
-
-
-if __name__ == '__main__':
-    print("")
-
-    ap = argparse.ArgumentParser(description="Plot TADM data from a .mdb file.",
-                                 epilog="Created by Joel Gruselius (github.com/jgruselius)")
-    ap.add_argument("infile", help="The TADM file to parse",
-                    type=file_exists)
-
-    lc_opt = ap.add_mutually_exclusive_group()
-    lc_opt.add_argument("-l", "--liquid", metavar="X", help="The liquid class name to plot")
-    lc_opt.add_argument("-i", "--interactive", action="store_true",
-                        help="Select the available liquid classes from a menu")
-    lc_opt.add_argument("-a", "--all", action="store_true", help="Save plots of all liquid classes")
-    lc_opt.add_argument("-P", "--par", action="store_true", help="Save all plots using multiprocessing")
-
-    save_opt = ap.add_mutually_exclusive_group()
-    save_opt.add_argument("-p", "--plot", type=dir_exists,
-                          help="Save the plot (png) to this file (full path, will overwrite)")
-    save_opt.add_argument("-o", "--outdir", metavar="DIR", type=dir_exists,
-                          help="Save the plots (png) to this directory (full path, will overwrite)")
-
-    ap.add_argument("-n", "--noshow", action="store_true",
-                    help="Don't show the plot window")
-    ap.add_argument("-v", "--verbose", action="store_true", required=False,
-                    help="Print more details about what's going on")
-    ap.add_argument("-L", "--lcdb", metavar="DB", type=file_exists, default=get_path("ML_STARLiquids.mdb"),
-                          help="Use some other liquid class definition database")
-
-    args = ap.parse_args()
-
-    loglevel = logging.DEBUG if args.verbose else logging.INFO
-    logging.basicConfig(level=loglevel, style="{", format="{message}",
-                        handlers=[RichHandler(show_time=False, markup=True, show_path=False)])
-    # At DEBUG level matplotlib spews out a lot of lines related to fonts. We want to ignore that:
-    logging.getLogger("matplotlib").setLevel(logging.INFO)
-
-    if not check_driver():
-        driver_help()
-        sys.exit(1)
-
-    if (args.all or args.par) and args.outdir is None:
-        logging.error("Options '--all' and '--par' requires option '--outdir' to be specified")
-        sys.exit(1)
-
-    if args.noshow and args.plot is None and args.outdir is None:
-        logging.warning("Plot window disabled AND no output path given (-p):\n"
-                        "Plotting for nothing!")
-
-    main(args)
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
